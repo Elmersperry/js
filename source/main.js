c19bc371ac9dff2c2a9556f6ac82afad525ce391
Elmersperry <rodionstudent@mail.ru>
2024-09-12 21:43:14 +0300

    123

--- source/main.js
+++ source/main.js
@@ -1,5 +1,7 @@
+/*
 alert("Hello from JavaScript!");
 confirm("Согласны, узнали?");
+*/
 /*
 undefined - неинициализированные значения
 null - флаг отсутствия значений (либо нулевое значение)
@@ -51,7 +53,7 @@ let last = input.slice(4);
 let res = last + input.slice(0, 4);
 alert("Ваше число теперь: " + res);
 */
-
+/*
 let x = Number(prompt("Введите первое число"));
 let y = Number(prompt("Введите второе число"));
 let S = x + y;
@@ -62,3 +64,58 @@ alert("Сумма" + S);
 alert("Умножение" + P);
 alert("Разница" + F);
 alert("Деление" + D);
+*/
+/*
+window.onload = function () {
+function foo() {
+let a = Number(prompt("Введите число"));
+let x = Number(prompt("Введите ещё одно число"));
+const k = 5;
+let y = k*x + a*(x**2);
+alert("Результат выполнения функции: " + y);
+}
+}
+*/
+
+function calc(a, b, operation) {
+  if (operation == "+") {
+    return a + b;
+  } else if (operation == "-") {
+    return a - b;
+  } else if (operation == "*") {
+    return a * b;
+  } else if (operation == "/") {
+    if (b == 0) {
+      return "Делить на ноль нельзя!";
+    } else {
+      return a / b;
+    }
+  } else {
+    return "Неизвестная операция. Попробуйте ещё раз";
+  }
+}
+
+
+function calcPaS() {
+  let x = Number(document.getElementsByName("num1")[0].value);
+  let y = Number(document.getElementsByName("num2")[0].value);
+  let op = document.getElementsByName("operation")[0].value;
+  console.log(x, y, op);
+  let res = calc(x, y, op);
+  alert("Результат операции: " + res);
+}
+
+window.onload = () => {
+  let buttonCalc = document.getElementById("calculator");
+  buttonCalc.addEventListener("click", calcPaS);
+};
+
+/*
+  let x = Number(prompt("Введите первое число"));
+  let y = Number(prompt("Введите второе число"));
+  let op = prompt("Введите тип операции (+, -, *, /)");
+  let res = calc(x, y, op);
+  alert("Результат вашей операции: " + res);
+};
+*/
+
